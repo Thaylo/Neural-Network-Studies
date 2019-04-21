a5f386e9c5ac14da09a1f57d76f66b5fb0436646
Thaylo Xavier <thayloxavier@gmail.com>
2019-04-20 21:05:21 -0300

    Feed forward

---
+++ Neural/NeuralNetwork.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class NeuralLayer:
+    input_tensor = []
+    weights = []
+    output_tensor = []
+
+    def __init__(self, input_dimension, weights_dimension, output_dimension):
+        self.input_tensor = np.random.rand(input_dimension[0], input_dimension[1])
+        self.weights = np.random.rand(weights_dimension[0], weights_dimension[1])
+        self.output_tensor = np.random.rand(output_dimension[0], output_dimension[1])
+
+    def input_x(self, input_data):
+        assert(input_data.shape == self.input_tensor.shape)
+        self.input_tensor = input_data.copy()
+
+    def update_state(self):
+        y = np.dot(self.weights, self.input_tensor)
+        assert(y.shape == self.output_tensor.shape)
+        self.output_tensor = y
+        return y
+
+
+class NeuralNetwork:
+    layers = []
+
+    def __init__(self, nn_configuration_list):
+        for nn_configuration in nn_configuration_list:
+            input_dimension = nn_configuration[0]
+            weights_dimension = nn_configuration[1]
+            output_dimension = nn_configuration[2]
+            self.layers.append(NeuralLayer(input_dimension, weights_dimension, output_dimension))
+
+    def feed_forward(self, input_data):
+        current_input = input_data
+        for layer in self.layers:
+            layer.input_x(current_input)
+            current_input = layer.update_state()
+        final_value = current_input
+        return final_value
+
+
+def sigmoid(x, derivative=False):
+    y = 1. / (np.exp(-x) + 1.)
+    if derivative:
+        return np.multiply(y, (1. - y))
+    return y
--- main.py
+++ main.py
@@ -1,48 +1,22 @@
 import numpy as np
 
-
-def sigmoid(x, derivative=False):
-    y = 1. / (np.exp(-x) + 1.)
-    if derivative:
-        return np.multiply(y, (1. - y))
-    return y
-
-
-def feed_forward(x, syn):
-    x_temp = x
-    for s in syn:
-        y_temp = sigmoid(x_temp)
-        x_temp = np.dot(s.T, y_temp)
-    return x_temp
-
-
-def compute_gradient(x, y, syn):
-    return 0
-
-
-def loss(y, d):
-    e = y-d
-    return np.linalg.norm(e)
+import Neural.NeuralNetwork as nN
 
 
 def main():
 
     np.random.seed(0)
-    syn0 = np.random.rand(2, 4)
-    syn1 = np.random.rand(4, 1)
-    syn = [syn0, syn1]
+    nn_configuration = []
 
-    input_data = np.fromfile("data/input", sep=' ').reshape(4, 2)
-    d = np.fromfile("data/output", sep=' ').reshape(4, 1)
+    layer_config_a = [(3, 1), (4, 3), (4, 1)]
+    layer_config_b = [(4, 1), (1, 4), (1, 1)]
 
-    print(d)
-    y = np.zeros(d.shape)
+    nn_configuration.append(layer_config_a)
+    nn_configuration.append(layer_config_b)
 
-    for i in range(d.shape[0]):
-        y[i] = feed_forward(input_data[i, :].reshape(2, 1), syn)
-
-    print(y)
-    print(loss(y, d))
+    nn = nN.NeuralNetwork(nn_configuration)
+    input_data = np.random.rand(3, 1)
+    nn.feed_forward(input_data)
 
 
 if __name__ == "__main__":
